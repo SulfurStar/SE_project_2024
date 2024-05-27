cb2fc391dd4ad0e6f6c1514ff821217e8e962514
SulfurStar <35754994+SulfurStar@users.noreply.github.com>
2024-05-28 01:42:23 +0800

    PS table

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <header>
-      <!-- 可以在這裡添加導航條或其他全局內容 -->
       <nav>
-        <NuxtLink to="/">Homeee</NuxtLink>
-        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/" class="title-homepage">高雄大學學生校外住宿管理系統</NuxtLink>
+        <NuxtLink to="/about" class="title-btn">About</NuxtLink>
       </nav>
     </header>
     <main>
@@ -12,6 +11,7 @@
     </main>
   </div>
 </template>
+
 
 <script setup>
 // 在這裡可以添加佈局級別的腳本設置
@@ -21,10 +21,25 @@
 /* 在這裡可以添加佈局級別的樣式 */
 nav a {
   margin: 0 10px;
-  color: #42b983;
+  color: #ffffff;
   text-decoration: none;
 }
 nav a.router-link-active {
   font-weight: bold;
+}
+header {
+  background-color: #35495e;
+  color: white;
+  padding: 10px 0;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+.title-homepage {
+  font-size: 2.5em;
+}
+
+.title-btn {
+  font-size: 1.5em;
+  float: right;
+  margin-right: 20px;
 }
 </style>
