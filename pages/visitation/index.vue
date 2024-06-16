f2535d6aacf2b9f36d801e70f1b39db772bf034c
SulfurStar <35754994+SulfurStar@users.noreply.github.com>
2024-06-17 01:07:19 +0800

    index

--- pages/visitation/index.vue
+++ pages/visitation/index.vue
@@ -57,7 +57,7 @@ const buttons = ref([
   { id: 4, name: '學生修改訪視紀錄', url: `/visitation/UpdateVisitRecordStudent/${user.value ? user.value.id : ''}` }, // 动态路由
   { id: 5, name: '學生查詢訪視紀錄', url: '/visitation/SearchVisitStudent' },
   { id: 6, name: '刪除訪視紀錄', url: '/visitation/DeleteVisitRecord' },
-  { id: 7, name: '老師查詢訪視紀錄', url: '/visitation/VisitCheckTeacher' },
+  { id: 7, name: '老師查詢訪視紀錄', url: `/visitation/VisitationCheck/${user.value ? user.value.id : ''}` },
   { id: 8, name: '管理員查詢訪視紀錄', url: '/visitation/VisitCheckAdmin' },
   { id: 9, name: '老師填寫訪視紀錄', url: '/visitation/FillVisitRecordTeacher' },
   { id: 10, name: '老師修改訪視紀錄', url: `/visitation/UpdateVisitRecordTeacher/${user.value ? user.value.id : ''}` },
